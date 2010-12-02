aa9b291a7a285cd9f3cdf767d2854608ada344d6
John Cupitt <jcupitt@gmail.com>
2010-12-02 12:36:53 +0000

    add 7.24 win32 build area

---
+++ 7.24/clean.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+basedir=`pwd`
+package=nip2
+version=7.22.3
+
+installdir=$basedir/inst
+packagedir=$basedir/packages
+repackagedir=$basedir/$package-$version
+checkoutdir=$basedir/checkout
+
+echo wiping install area $installdir
+
+rm -rf $installdir
+mkdir $installdir
+
+echo cleaning checkout area $checkoutdir
+
+for i in $checkoutdir/*; do
+	if [ -d $i ]; then
+		rm -rf $i
+	fi
+done
+
+echo cleaning misc files
+
+rm -f $basedir/*.zip
+rm -f $basedir/*.exe
+
+rm -rf $repackagedir
+
+rm -rf nsis/$package-$version
+rm -f nsis/$package-$version-setup.exe
+
+
+
