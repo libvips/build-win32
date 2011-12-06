a87485e992641387af1e181ee5057b2f4ffd4102
John Cupitt <jcupitt@gmail.com>
2011-12-06 17:21:49 +0000

    sync

--- 7.26/variables.sh
+++ 7.26/variables.sh
@@ -1,10 +1,10 @@
 # includes by all scripts .. edit versions here, and in vips.modules
 
 nip2_package=nip2
-nip2_version=7.26.2
+nip2_version=7.26.3
 
 vips_package=vips
-vips_version=7.26.2
+vips_version=7.26.3
 
 # stuff is in here
 basedir=`pwd`
