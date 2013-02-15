0faeb4107b8fc1d6cd3e04c50f3516c60f90c3fd
John Cupitt <jcupitt@gmail.com>
2013-02-15 20:05:40 +0000

    add 7.32

---
+++ 7.32/variables.sh
@@ -0,0 +1,20 @@
+# included by all scripts .. edit versions here, and in vips.modules
+
+nip2_package=nip2
+nip2_version=7.32.0
+
+vips_package=vips
+vips_version=7.32.0
+
+# stuff is in here
+basedir=`pwd`
+
+# download zips to here
+packagedir=packages
+
+# unzip to here
+installdir=inst
+
+# jhbuild will download sources to here 
+checkoutdir=checkout
+
