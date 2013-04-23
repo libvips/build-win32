2a9991522598c9fdf9869ee28f4dec7b64dd472c
John Cupitt <jcupitt@gmail.com>
2013-04-23 14:43:59 +0100

    update for stable

--- 7.32/variables.sh
+++ 7.32/variables.sh
@@ -1,10 +1,10 @@
 # included by all scripts .. edit versions here, and in vips.modules
 
 nip2_package=nip2
-nip2_version=7.32.0
+nip2_version=7.32.1
 
 vips_package=vips
-vips_version=7.32.0
+vips_version=7.32.2
 
 # stuff is in here
 basedir=`pwd`
